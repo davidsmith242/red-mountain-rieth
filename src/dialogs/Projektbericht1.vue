e70a008d472e74325578b55a50317a8b9ff638bd
David Schmitt <david.schmitt@had-websolutions.de>
2022-06-21 22:35:50 +0200

    HTML Fehler in Projektbericht behoben (<v-card-text> und <v-card-title> darf es nur einmal geben)

--- src/dialogs/Projektbericht1.vue
+++ src/dialogs/Projektbericht1.vue
@@ -2,7 +2,7 @@
   <div>
     <v-dialog
       v-model="dialog"
-      width="550"
+      width="600"
     >
       <template v-slot:activator="{ on, attrs }">
         <v-btn
@@ -21,39 +21,90 @@
         </v-card-title>
 
         <v-card-text>
-          Ausgangslage: Eine teilstationäre Hilfe zur Erziehung in einer Gruppe mit 8 Kindern im Alter von 7-14 Jahren sowie 2 Betreuern. Es besteht eine feste Wochenstruktur mit einer Betreuung bis 17 Uhr. Das Projekt soll alle 14 Tage stattfinden, untergliedert in folgende Module:
-          Gesunde Ernährung; Sport & Fitness; Körperliche Hygiene; Mobbing/Ausgrenzung/Gewalt; Gefühle erkennen/Emotionen wahrnehmen; Entspannungsübungen
-        </v-card-text>
-        <v-card-text>
-          Ziel: Stärkung der körperlichen und geistigen Gesundheit sowie der Erwerb von neuem Wissen und der gleichzeitigen Stärkung des Selbstwertgefühls. Am  Ende  des  Projektes  werden  die  einzelnen  Bereiche  zusammengeführt,  so dass   für   die   Kinder   sich   ein   Gesamtbild   ergibt,   welches   ihnen   das Zusammenwirkung  von  einem  gesunden  Geist  und einem  gesunden Körper vermittelt.
-        </v-card-text>
+          <span class="text-subtitle-2">Ausgangslage:</span> 
+          <p>
+            Eine teilstationäre Hilfe zur Erziehung in einer Gruppe mit 8 Kindern im Alter von 7-14 Jahren 
+            sowie 2 Betreuern. Es besteht eine feste Wochenstruktur mit einer Betreuung bis 17 Uhr. Das Projekt soll alle 
+            14 Tage stattfinden, untergliedert in folgende Module:
+            Gesunde Ernährung; Sport & Fitness; Körperliche Hygiene; Mobbing/Ausgrenzung/Gewalt; Gefühle erkennen/Emotionen 
+            wahrnehmen; Entspannungsübungen
+            <br />
+            Ziel: Stärkung der körperlichen und geistigen Gesundheit sowie der Erwerb von neuem Wissen und der 
+            gleichzeitigen Stärkung des Selbstwertgefühls. Am  Ende des  Projektes  werden  die  einzelnen Bereiche 
+            zusammengeführt, sodass sich für die Kinder ein Gesamtbild ergibt, welches ihnen das Zusammenwirkung 
+            von einem gesunden Geist und einem gesunden Körper vermittelt.
+          </p>
 
-        <v-card-text>Zeitraum: 01.09.2023 - 31.08.2024 </v-card-text>
+          <div class="text-h6"> Zeitraum: 01.09.2023 - 31.08.2024</div>
 
-        <v-card-text><v-card-title class="text-h5 grey lighten-2">
-          Modul 1 - Gesunde Ernährung
-        </v-card-title>Dies spielt in der Tagesgruppe eine wichtige Rolle. Jeden Tag bereiten die  Kinder  eine  kleine  Mahlzeit  zu. In  den  Projektstunden  werden die einzelnen  Nahrungsmittel  genauer  angeschaut.  Deren Inhaltsstoffe,  wie  sie  auf den  Körper  einwirken,  welche  Lebensmittel  unverzichtbar  sind  und  was  der Körper weniger gut verarbeiten kann. Dazu werden wir eine Lebensmittelberater*in einladen, welche bei dem theoretischen Teil unterstützen wird.  Um  den  Kindern  noch  mehr  zu  verdeutlichen, wo  die  Lebensmittel herkommen, werden sie sich auf einem Bauernhof/in der Landwirtschaft genauer umschauen. Die  Kinder  sollen  auch  ein  Gefühl  dafür  bekommen,  was  Lebensmittel  kosten. Dies lässt sich beim Einkaufen und einem gezielten Preisvergleich ermöglichen. Sie werden aus einfachen Grundnahrungsmitteln schmackhafte und vielseitige Gerichte herstellen, um ein Bewusstsein für materielle Werte zu schaffen. </v-card-text>
-        
-        <v-card-text><v-card-title class="text-h5 grey lighten-2">
-          Modul 2 - Sport & Fitness
-        </v-card-title>In der Wochenstruktur sind Sport und körperliche Fitness ein Bestandteil der  Gruppenaktivitäten. Während des  Projektes  werden  die Kinder herausarbeiten, warum Sport für die Gesundheit des Körpers wichtig ist. Neben Fußballspiel und Fahrrad  fahren  sollen auch andere  sportliche  Aktivitäten  angeschaut werden,  die  im Gruppenalltag  ansonsten  nicht  umsetzbar  sind.  Wir möchten einen Grundkurs   im  Klettern anbieten,   einen   Schwimmkurs   belegen  lassen und   die vielseitigen  Möglichkeiten  des  Fitnessstudios  näher bringen.  Die  Kinder  sollen erleben, dass sportliche Bewegung Spaß macht.  </v-card-text>
-        
-        <v-card-text><v-card-title class="text-h5 grey lighten-2">
-          Modul 3 - Körperliche Hygiene
-        </v-card-title>Zur  Gesundheit  gehört  auch  die körperliche  Pflege  und  Hygiene.  Für  die Kinder  in der  Tagesgruppe  ist  es  oft  nicht  selbstverständlich, sich  täglich  die Zähne zu putzen oder sich zu waschen. Daher ist es wichtig, auch diese Aspekte zu   vermitteln. Dazu   ist   geplant,dass wir kompetente   Fachkräfte   mit entsprechender Profession einladen, die diese Themen den Kindern spielerisch vermitteln.   </v-card-text>
-        
-        <v-card-text><v-card-title class="text-h5 grey lighten-2">
-          Modul 4 - Mobbing/Ausgrenzung/Gewalt
-        </v-card-title>Ein ungepflegter Körper kann auch zur Ausgrenzung und zu Mobbing führen. Dies  möchten  wir  im  nächsten  Teil  des  Projektes  behandeln.  Die Kinder  werden sich damit beschäftigen, wie es zu Mobbing kommt, was man tun kann, wenn man betroffen ist und wie man helfen kann,dass Mobbing gar nicht erst entsteht. Dazu werden wir  einen  Antiaggressionslehrer  einladen,  der  sowohl  theoretisch  als auch  praktische  Übungen  mit  den  Kindern  durchführen  wird.  Damit  die  Kinder Strategien  erlernen, wie  sie  Gewalt  verhindern  können  und  deeskalierend  in Situationen reagieren können und dadurch ihre Resilienzen gestärkt werden.  </v-card-text>
-        
-        <v-card-text><v-card-title class="text-h5 grey lighten-2">
-          Modul 5 - Gefühle erkennen und Emotionen wahrnehmen
-        </v-card-title>Um zu lernen, wie es zu krisenbehafteten Situationen kommt, ist es wichtig,über die Gefühle  und  Emotionen zu  sprechen,  die  solche Situationen  auslösen. Häufig  können  die  Kinder  sich  nicht  adäquat  ausdrücken  oder  Gefühle  des Gegenübers  richtig  einschätzen.  Spielerisch  werden  die Kinder erarbeiten, was  für Gefühle es gibt, wie diese ausgedrückt werden und wie man sie erkennt. Dazu werden  pädagogische  Fachkräfte von  anderen freien Trägern eingeladen,  die  sich mit dem Thema "Nähe, Distanz, Körpergrenzen" intensiv beschäftigen. Um ein Gefühl für „gute Laune“ und wie sich diese anfühlt zu bekommen, werden wir eine „Lachstunde“ organisieren. Unter fachkundiger Anleitung wird eine Stunde herzhaft gelacht.   </v-card-text>
-        
-        <v-card-text><v-card-title class="text-h5 grey lighten-2">
-          Modul 6 - Entspannungsübungen
-        </v-card-title>Das Projekt wird mit verschiedenen Entspannungstechniken, welche die Kinder im letzten Modul kennenlernen, abgeschlossen. Dazu werden wir mit den Krankenkassen zusammenarbeiten, um einen entsprechenden Kurs durchführen zu können.  </v-card-text>
-        
+          <div class="text-h5 grey lighten-2">
+            Modul 1 - Gesunde Ernährung
+          </div>
+          <p>
+            Dies spielt in der Tagesgruppe eine wichtige Rolle. Jeden Tag bereiten die  Kinder  eine  kleine  Mahlzeit  zu. 
+            In  den  Projektstunden  werden die einzelnen  Nahrungsmittel genauer  angeschaut.  Deren Inhaltsstoffe,  
+            wie  sie  auf den  Körper  einwirken,  welche  Lebensmittel unverzichtbar  sind  und  was  der Körper weniger 
+            gut verarbeiten kann. Dazu werden wir eine Lebensmittelberater*in einladen, welche bei dem theoretischen Teil 
+            unterstützen wird.  Um  den  Kindern  noch  mehr  zu  verdeutlichen, wo die  Lebensmittel herkommen, werden 
+            sie sich auf einem Bauernhof/in der Landwirtschaft genauer umschauen. Die  Kinder sollen auch ein Gefühl 
+            dafür  bekommen,  was  Lebensmittel  kosten. Dies lässt sich beim Einkaufen und einem gezielten Preisvergleich 
+            ermöglichen. Sie werden aus einfachen Grundnahrungsmitteln schmackhafte und vielseitige Gerichte herstellen, 
+            um ein Bewusstsein für materielle Werte zu schaffen.
+          </p>
+          
+          <div class="text-h5 grey lighten-2">
+            Modul 2 - Sport & Fitness
+          </div>
+          <p>
+            In der Wochenstruktur sind Sport und körperliche Fitness ein Bestandteil der  Gruppenaktivitäten. 
+            Während des  Projektes  werden  die Kinder herausarbeiten, warum Sport für die Gesundheit des Körpers 
+            wichtig ist. Neben Fußballspiel und Fahrrad fahren sollen auch andere sportliche Aktivitäten angeschaut werden, 
+            die im Gruppenalltag ansonsten nicht umsetzbar  sind. Wir möchten einen Grundkurs im Klettern anbieten, einen 
+            Schwimmkurs belegen lassen und die vielseitigen Möglichkeiten des Fitnessstudios näher bringen.  Die 
+            Kinder  sollen erleben, dass sportliche Bewegung Spaß macht.
+          </p>
+          <div class="text-h5 grey lighten-2">
+            Modul 3 - Körperliche Hygiene
+          </div>
+          <p>
+            Zur  Gesundheit  gehört  auch  die körperliche  Pflege  und  Hygiene.  Für  die Kinder  in der  
+            Tagesgruppe  ist  es  oft  nicht  selbstverständlich, sich  täglich  die Zähne zu putzen oder sich zu waschen. 
+            Daher ist es wichtig, auch diese Aspekte zu vermitteln. Dazu ist geplant,dass wir kompetente Fachkräfte mit 
+            entsprechender Profession einladen, die diese Themen den Kindern spielerisch vermitteln.
+          </p>
+          <div class="text-h5 grey lighten-2">
+            Modul 4 - Mobbing/Ausgrenzung/Gewalt
+          </div>
+          <p>
+            Ein ungepflegter Körper kann auch zur Ausgrenzung und zu Mobbing führen. Dies  möchten  wir  im  
+            nächsten  Teil  des  Projektes  behandeln.  Die Kinder  werden sich damit beschäftigen, wie es zu Mobbing 
+            kommt, was man tun kann, wenn man betroffen ist und wie man helfen kann,dass Mobbing gar nicht erst entsteht. 
+            Dazu werden wir  einen  Antiaggressionslehrer  einladen,  der  sowohl  theoretisch  als auch  praktische  
+            Übungen  mit  den  Kindern  durchführen  wird.  Damit  die  Kinder Strategien  erlernen, wie  sie  Gewalt  
+            verhindern  können  und  deeskalierend  in Situationen reagieren können und dadurch ihre Resilienzen gestärkt 
+            werden.
+          </p>
+          <div class="text-h5 grey lighten-2">
+            Modul 5 - Gefühle erkennen und Emotionen wahrnehmen
+          </div>
+          <p>
+            Um zu lernen, wie es zu krisenbehafteten Situationen kommt, ist es wichtig,über die Gefühle  und  Emotionen 
+            zu  sprechen,  die  solche Situationen  auslösen. Häufig  können  die  Kinder  sich  nicht  adäquat  ausdrücken  
+            oder  Gefühle  des Gegenübers  richtig  einschätzen.  Spielerisch  werden  die Kinder erarbeiten, was  für Gefühle 
+            es gibt, wie diese ausgedrückt werden und wie man sie erkennt. Dazu werden  pädagogische  Fachkräfte von  anderen 
+            freien Trägern eingeladen,  die  sich mit dem Thema "Nähe, Distanz, Körpergrenzen" intensiv beschäftigen. Um ein 
+            Gefühl für „gute Laune“ und wie sich diese anfühlt zu bekommen, werden wir eine „Lachstunde“ organisieren. Unter 
+            fachkundiger Anleitung wird eine Stunde herzhaft gelacht.
+          </p>
+          <div class="text-h5 grey lighten-2">
+            Modul 6 - Entspannungsübungen
+          </div>
+          <p>
+            Das Projekt wird mit verschiedenen Entspannungstechniken, welche die Kinder im letzten Modul kennenlernen, 
+            abgeschlossen. Dazu werden wir mit den Krankenkassen zusammenarbeiten, um einen entsprechenden Kurs durchführen 
+            zu können.
+          </p>
+        </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn
--- src/projekte/SofioterStrasse.vue
+++ src/projekte/SofioterStrasse.vue
@@ -1,10 +1,14 @@
 <template>
   <Projekt titel="Moskauer Platz / 01">
     <h3 class="text-h5">Tagesgruppe "Sofioter Straße"</h3>
-    <p class="text-body1">Die Tagesgruppe ist eine Hilfe zur Erziehung. Acht Schulkinder im Alter von 6 bis 14 Jahren werden in einem geschützten Rahmen betreut.
-    Die Familien erhalten durch die Tagesgruppe eine umfangreiche Unterstützung, wenn die soziale Entwicklung ihrer Kinder gefährdet ist. Sozialpädagogische Gruppenangebote und individuelle Einzelbetreuung unterstützen das soziale Lernen in der Gruppe. Die gezielte Förderung orientiert sich an den im Hilfeplan verankerten Zielen, die durch die Eltern, Betreuer und Jugendamtsmitarbeiter gemeinsam vereinbart werden.
+    <p class="text-body1">
+      Die Tagesgruppe ist eine Hilfe zur Erziehung. Acht Schulkinder im Alter von 6 bis 14 Jahren 
+      werden in einem geschützten Rahmen betreut.
+      Die Familien erhalten durch die Tagesgruppe eine umfangreiche Unterstützung, wenn die soziale Entwicklung ihrer Kinder 
+      gefährdet ist. Sozialpädagogische Gruppenangebote und individuelle Einzelbetreuung unterstützen das soziale Lernen in der 
+      Gruppe. Die gezielte Förderung orientiert sich an den im Hilfeplan verankerten Zielen, die durch die Eltern, 
+      Betreuer und Jugendamtsmitarbeiter gemeinsam vereinbart werden.
     </p>
-    
   </Projekt>
 </template>
 
