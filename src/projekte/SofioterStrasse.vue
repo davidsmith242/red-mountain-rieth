2893ecb29ca23f975365ab333431619da0dbd9b1
Jenner2382 <107841495+Jenner2382@users.noreply.github.com>
2022-07-02 18:39:31 +0200

    Update SofioterStrasse.vue

--- src/projekte/SofioterStrasse.vue
+++ src/projekte/SofioterStrasse.vue
@@ -3,11 +3,8 @@
     <h3 class="text-h5">Tagesgruppe "Sofioter Straße"</h3>
     <p class="text-body1">
       Die Tagesgruppe ist eine Hilfe zur Erziehung. Acht Schulkinder im Alter von 6 bis 14 Jahren 
-      werden in einem geschützten Rahmen betreut.
-      Die Familien erhalten durch die Tagesgruppe eine umfangreiche Unterstützung, wenn die soziale Entwicklung ihrer Kinder 
-      gefährdet ist. Sozialpädagogische Gruppenangebote und individuelle Einzelbetreuung unterstützen das soziale Lernen in der 
-      Gruppe. Die gezielte Förderung orientiert sich an den im Hilfeplan verankerten Zielen, die durch die Eltern, 
-      Betreuer und Jugendamtsmitarbeiter gemeinsam vereinbart werden.
+      werden in einem geschützten Rahmen betreut. Die Familien erhalten durch die Tagesgruppe eine umfangreiche Unterstützung, wenn die soziale Entwicklung ihrer Kinder 
+      gefährdet ist. 
     </p>
     <Projektbericht1 />
   </Projekt>
